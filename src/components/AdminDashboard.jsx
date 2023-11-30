c36cf689e1a5a616beb3acf15076e33df1842167
samrat53 <samrat05hccs@gmail.com>
2023-11-30 14:44:24 +0530

    build admin dashboard layout

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
 
 const AdminDashboard = () => {
   return (
-    <div>AdminDashboard</div>
-  )
-}
+    <>
+      <div className="submission-form rounded-sm flex flex-col items-center justify-center">
+      <h1 className="py-5">Admin Dashboard</h1>
+        <details className="dropdown">
+          <summary className="m-1 btn rounded-lg cursor-pointer bg-green-200 hover:bg-green-500 hover:text-white ">
+            Domain Select
+          </summary>
+          <ul className="p-2 shadow menu dropdown-content z-[1] rounded-box w-52 bg-blue-100">
+            <li>
+              <a>Web Development</a>
+            </li>
+            <li>
+              <a>Android Development</a>
+            </li>
+            <li>
+              <a>AI / ML</a>
+            </li>
+          </ul>
+        </details>
 
-export default AdminDashboard
+        <label className="form-control w-full max-w-xs my-2">
+          <div className="label">
+            <span className="label-text">Drive Link</span>
+            <span className="label-text-alt text-green-600">*</span>
+          </div>
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+          />
+          <div className="label">
+            <span className="label-text">Live Link</span>
+            <span className="label-text-alt text-blue-600">#</span>
+          </div>
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+          />
+          <div className="label">
+            <span className="label-text-alt">Mandatory: <span className="text-green-600">*</span></span>
+            <span className="label-text-alt">Optional: <span className="text-blue-600">#</span></span>
+          </div>
+        </label>
+        <button className="btn">Submit</button>
+      </div>
+    </>
+  );
+};
+
+export default AdminDashboard;
